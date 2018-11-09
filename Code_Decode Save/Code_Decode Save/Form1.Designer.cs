467c5ccc75cd3a379a684ae7a424fa2d94e57a0e
Ricardo Petrére <ricardopetrere@gitlab.com>
2018-11-09 21:55:29 -0200

    Adicionado projeto de codificar e decodificar save

---
+++ Code_Decode Save/Code_Decode Save/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Code_Decode_Save
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoadFile = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFileContent = new System.Windows.Forms.TextBox();
+            this.btnDecode = new System.Windows.Forms.Button();
+            this.btnEncode = new System.Windows.Forms.Button();
+            this.txtDecodedContent = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnLoadFile
+            // 
+            this.btnLoadFile.Location = new System.Drawing.Point(12, 12);
+            this.btnLoadFile.Name = "btnLoadFile";
+            this.btnLoadFile.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadFile.TabIndex = 0;
+            this.btnLoadFile.Text = "Load File";
+            this.btnLoadFile.UseVisualStyleBackColor = true;
+            this.btnLoadFile.Click += new System.EventHandler(this.btnLoadFile_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Or input .sav file content here:";
+            // 
+            // txtFileContent
+            // 
+            this.txtFileContent.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtFileContent.Location = new System.Drawing.Point(12, 54);
+            this.txtFileContent.Multiline = true;
+            this.txtFileContent.Name = "txtFileContent";
+            this.txtFileContent.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtFileContent.Size = new System.Drawing.Size(260, 329);
+            this.txtFileContent.TabIndex = 3;
+            // 
+            // btnDecode
+            // 
+            this.btnDecode.Location = new System.Drawing.Point(278, 12);
+            this.btnDecode.Name = "btnDecode";
+            this.btnDecode.Size = new System.Drawing.Size(75, 23);
+            this.btnDecode.TabIndex = 4;
+            this.btnDecode.Text = "Decode";
+            this.btnDecode.UseVisualStyleBackColor = true;
+            this.btnDecode.Click += new System.EventHandler(this.btnDecode_Click);
+            // 
+            // btnEncode
+            // 
+            this.btnEncode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEncode.Location = new System.Drawing.Point(504, 12);
+            this.btnEncode.Name = "btnEncode";
+            this.btnEncode.Size = new System.Drawing.Size(75, 23);
+            this.btnEncode.TabIndex = 5;
+            this.btnEncode.Text = "Encode";
+            this.btnEncode.UseVisualStyleBackColor = true;
+            this.btnEncode.Click += new System.EventHandler(this.btnEncode_Click);
+            // 
+            // txtDecodedContent
+            // 
+            this.txtDecodedContent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDecodedContent.Location = new System.Drawing.Point(279, 42);
+            this.txtDecodedContent.Multiline = true;
+            this.txtDecodedContent.Name = "txtDecodedContent";
+            this.txtDecodedContent.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtDecodedContent.Size = new System.Drawing.Size(300, 341);
+            this.txtDecodedContent.TabIndex = 6;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Save Files|*.sav";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "sav";
+            this.saveFileDialog1.Filter = "Save Files|*.sav";
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(94, 13);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.ReadOnly = true;
+            this.txtFileName.Size = new System.Drawing.Size(178, 20);
+            this.txtFileName.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 395);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.txtDecodedContent);
+            this.Controls.Add(this.btnEncode);
+            this.Controls.Add(this.btnDecode);
+            this.Controls.Add(this.txtFileContent);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnLoadFile);
+            this.MinimumSize = new System.Drawing.Size(460, 250);
+            this.Name = "Form1";
+            this.Text = "Encode/Decode Save Files";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFileContent;
+        private System.Windows.Forms.Button btnDecode;
+        private System.Windows.Forms.Button btnEncode;
+        private System.Windows.Forms.TextBox txtDecodedContent;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox txtFileName;
+    }
+}
+
---
+++ Code_Decode Save/Code_Decode Save/Form1.cs
@@ -0,0 +1,83 @@
+﻿using Lzf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Code_Decode_Save
+{
+    public partial class Form1 : Form
+    {
+        /*  Credits to
+         *  https://www.kongregate.com/forums/4721-general/topics/423987-how-to-decrypt-the-export-string
+         *  and 
+         *  http://forum.unity3d.com/threads/lzf-compression-and-decompression-for-unity.152579/
+        */
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        String[] usefulData;//0 = JSON content, 1 = MD5 Hash of JSON content
+
+        private void btnLoadFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtFileContent.Text = File.ReadAllText(openFileDialog1.FileName).Replace("\0", "");
+                txtFileName.Text = openFileDialog1.SafeFileName;
+                btnDecode.PerformClick();
+            }
+        }
+
+        private void btnEncode_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //MD5 creation is missing because the "Something else" is missing from the Decode side.
+                int amountConverted = 0;
+                byte[] input = Encoding.UTF8.GetBytes(txtDecodedContent.Text);
+                byte[] output = new byte[input.Length];
+                while (amountConverted == 0)
+                {
+                    output = new byte[output.Length * 2];
+                    amountConverted = LZF.Compress(input, input.Length, output, output.Length);
+                }
+                usefulData[0] = "." + Convert.ToBase64String(output);
+                File.WriteAllText(saveFileDialog1.FileName, usefulData[0] + "|" + usefulData[1]);
+            }
+        }
+
+        private void btnDecode_Click(object sender, EventArgs e)
+        {
+            if (txtFileContent.TextLength > 0)
+            {
+                usefulData = txtFileContent.Text.Substring(1).Split('|');
+                if (usefulData.Length != 2)//Not gonna happen, but in case it does...
+                {
+                    MessageBox.Show("Error while loading content");
+                    return;
+                }
+                byte[] bytearraydecoded = Convert.FromBase64String(usefulData[0]);
+                string base64decoded = Encoding.UTF8.GetString(bytearraydecoded);
+                byte[] output = new byte[bytearraydecoded.Length];
+                int amountConverted = 0;
+                while (amountConverted == 0)
+                {
+                    output = new byte[output.Length * 2];
+                    amountConverted = LZF.Decompress(bytearraydecoded, bytearraydecoded.Length, output, output.Length);
+                }
+                //Check MD5. But it's missing some information on the original thread...
+                txtDecodedContent.Text = Encoding.ASCII.GetString(output);
+            }
+            else
+            {
+                MessageBox.Show("Please input some text");
+            }
+        }
+    }
+}
